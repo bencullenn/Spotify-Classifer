13142c69c29ce17da18e6308887ccab5d27a6e7b
Ben Cullen <bencullenn@gmail.com>
2017-05-04 10:27:50 -0400

    Training Set Generator Added

---
+++ trainingSetGenerator.py
@@ -0,0 +1,4 @@
+"""
+This class takes a link to a Spotify playlist and converts that playlist to a training set 
+which can be used to train machine learning algorithms in the form of a text file
+"""
