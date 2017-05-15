b31f37d152ce7427d1f550f12fa985ee87d138be
Ben Cullen <bencullenn@gmail.com>
2017-05-15 16:57:26 -0400

    Added a class to train and test the classification algorithm

---
+++ PlaylistClassifer.py
@@ -0,0 +1,7 @@
+"""
+This class takes two text files of JSON Formatted data for a Spotify playlist and 
+trains a classification algorithm based on the data in the "Training Set" text file. 
+
+The algorithm is then tested with the "Test Set" text file and the accuracy score is output. 
+The algorithm can then be used give the fit of a link to any song on Spotify
+"""
