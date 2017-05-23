f3fb5bb752968d1296e11b2e815c09478761e538
Ben Cullen <bencullenn@gmail.com>
2017-05-23 14:53:47 -0400

    Added code that turns training labels into numerical values, uses training labels and training features to train a support vector machine classifier, uses the classifier to predict the labels of the test data, and then turns the labels back into strings which it prints out to the console.

--- PlaylistClassifer.py
+++ PlaylistClassifer.py
@@ -9,8 +9,10 @@ The algorithm can then be used give the fit of a link to any song on Spotify
 import pandas as pd
 import numpy as np
 import random
-from sklearn import svm
+import sklearn
 import pprint
+from sklearn import preprocessing
+from sklearn import svm
 
 """
 Functions
@@ -43,18 +45,18 @@ def createTestSet(dataSet, testSetProportion):
         if i % 10 == 0:
 
             # Take the row features and label and add it to the test set
-            print ("{}{}".format("Features added to Test Set", rowFeatures))
+            # print ("{}{}".format("Features added to Test Set", rowFeatures))
             testSetFeatures = np.append(testSetFeatures, [rowFeatures], axis=0)
-            print ("{}{}".format("Label added to Test Set:", rowLabel))
-            testSetLabels = np.append(testSetLabels, [rowLabel], axis=0)
+            # print ("{}{}".format("Label added to Test Set:", rowLabel))
+            testSetLabels = np.append(testSetLabels, [rowLabel])
             
         else:
 
             # Take the row features and label and add it to the training set
-            print ("{}{}".format("Features added to Train Set", rowFeatures))
+            # print ("{}{}".format("Features added to Train Set", rowFeatures))
             trainSetFeatures = np.append(trainSetFeatures, [rowFeatures], axis=0)
-            print ("{}{}".format("Label added to Train Set:", rowLabel))
-            trainSetLabels = np.append(trainSetLabels, [rowLabel], axis=0)
+            # print ("{}{}".format("Label added to Train Set:", rowLabel))
+            trainSetLabels = np.append(trainSetLabels, [rowLabel])
 
     # Print out some data to verify that the training and test sets are proper size
     print ("{}{}".format("DataSet Size:", len(dataSet)))
@@ -62,7 +64,7 @@ def createTestSet(dataSet, testSetProportion):
     print ("{}{}".format("TestSetFeatures Size:", len(testSetFeatures)))
     print ("{}{}".format("TrainSetLabels Size:", len(trainSetLabels)))
     print ("{}{}".format("TrainSetFeatures Size:", len(trainSetFeatures)))
-
+    print "\n"
     return testSetFeatures, testSetLabels, trainSetFeatures, trainSetLabels
 
 """
@@ -75,13 +77,37 @@ testFeatures, testLabels, trainFeatures, trainLabels = createTestSet(data, .10)
 
 print "Train Features"
 print(trainFeatures)
+print "\n"
 
 print "Train Labels"
 print(trainLabels)
+print "\n"
 
 print "Test Features"
 print(testFeatures)
+print "\n"
 
 print "Test Labels"
 print(testLabels)
+print "\n"
 
+# Normalize labels
+
+le = preprocessing.LabelEncoder()
+
+trainLabels = le.fit_transform(trainLabels)
+print "Labels Transformed\n"
+
+# Time to train some Classifers
+
+svmClf = svm.SVC()
+
+svmClf.fit(trainFeatures, trainLabels)
+print "Support Vector Machine Trained\n"
+
+# Calculate Accuracy
+
+svmPredictionData = svmClf.predict(testFeatures)
+svmPredictions = le.inverse_transform(svmPredictionData)
+
+print "{}{}".format("SVM Predictions", svmPredictions)
