49d1426dccabcae645cc5cba9a0360d058dce4cd
Ben Cullen <bencullenn@gmail.com>
2017-05-30 10:01:16 -0400

    Converted all function, argument, and variable names in TrainingSetGenerator.py from camel case to underscore separation to be consistent with Python's naming conventions

--- TrainingSetGenerator.py
+++ TrainingSetGenerator.py
@@ -14,7 +14,7 @@ Functions
 '''
 
 
-def getUsername():
+def get_username():
     if len(sys.argv) > 1:
         # Ask the user for their username
         return sys.argv[1]
@@ -24,7 +24,7 @@ def getUsername():
         sys.exit()
 
 
-def createTokenForScope(username, scope):
+def create_token_for_scope(username, scope):
     # Get token for user
     token = util.prompt_for_user_token(username, scope)
 
@@ -36,87 +36,87 @@ def createTokenForScope(username, scope):
         print "Can't get token for", username
 
 
-def getPlaylistForID(playlistID):
+def get_playlist_for_id(playlist_id):
     # Retrieve the playlist data in JSON format
-    return sp.user_playlist(authorizationUsername, playlistID)
+    return sp.user_playlist(authorization_username, playlist_id)
 
 
-def getTracksFromPlaylist(playlistID):
+def get_tracks_from_playlist(playlist_id):
     # Retrieve the playlist data in JSON format
-    playlist = getPlaylistForID(playlistID)
+    playlist = get_playlist_for_id(playlist_id)
 
     # Create two list objects, one to store all of JSON data for the tracks in the playlist
     # and another to store the uri link data for audio feature processing
     tracks = []
 
     # Get the total number of tracks in the playlist and create a variable to manage the offset value
-    playlistNumOfTracks = playlist['tracks']['total']
-    offset = 0
+    playlist_num_of_tracks = playlist['tracks']['total']
+    offset_value = 0
 
     # While the tracks list contains less than the number of tracks
     # in the playlist add chunks of 100 songs at a time to the tracks playlist
-    while len(tracks) < playlistNumOfTracks:
-        linkListSubset = sp.user_playlist_tracks(authorizationUsername, playlistID, fields='items', offset=offset)['items']
-        for track in linkListSubset:
+    while len(tracks) < playlist_num_of_tracks:
+        link_list_subset = sp.user_playlist_tracks(authorization_username, playlist_id, fields='items', offset=offset_value)['items']
+        for track in link_list_subset:
             tracks.append(track)
-        offset += 1
-        print("{}{}".format("Length of playlist:", playlistNumOfTracks))
+        offset_value += 1
+        print("{}{}".format("Length of playlist:", playlist_num_of_tracks))
         print("{}{}".format("Current amount of tracks retrieved:", len(tracks)))
-        print("{}{}".format("Offset Value:", offset))
+        print("{}{}".format("Offset Value:", offset_value))
     return tracks
 
 
-def getAudioFeaturesForPlaylistID(playlistID):
-    tracks = getTracksFromPlaylist(playlistID)
+def get_audio_features_for_playlist_id(playlist_id):
+    tracks = get_tracks_from_playlist(playlist_id)
     links = []
 
     for track in tracks:
         links.append(track['track']['uri'])
 
     # Create variables to store the beginning and end indies to enable getting subsets of results
-    startIndex = 0
-    endIndex = 100
-    audioFeatures = []
+    start_index = 0
+    end_index = 100
+    audio_features = []
 
     # While the amount of audio feature data is less than the amount of links
-    while len(audioFeatures) < len(links):
+    while len(audio_features) < len(links):
 
         # Adjust the end index to be at max the size of the list of links to avoid an index out of bounds error
-        if len(links) < endIndex:
-            endIndex = len(links)
-            print("{}{}".format("End index updated to:", endIndex))
+        if len(links) < end_index:
+            end_index = len(links)
+            print("{}{}".format("End index updated to:", end_index))
 
         # Create a temporary list
-        linkListSubset = []
+        link_list_subset = []
 
         # For the range of indexes remove the links from the link list and add them to the temp list
-        for index in range(startIndex, endIndex):
-            linkListSubset.append(links[index])
+        for index in range(start_index, end_index):
+            link_list_subset.append(links[index])
 
         # Retrieve the audio features for the subset of the links
-        audioFeaturesRawData = sp.audio_features(linkListSubset)
+        audio_features_raw_data = sp.audio_features(link_list_subset)
 
         # Add the raw data for each track to the audio features list
-        for item in audioFeaturesRawData:
-            audioFeatures.append(item)
+        for item in audio_features_raw_data:
+            audio_features.append(item)
 
         # Change the indices to include the next 100 songs
-        startIndex = endIndex
-        endIndex += 100
+        start_index = end_index
+        end_index += 100
 
         # Print some information to know what's happening
-        print("{}{}".format("Amount of links and audio features are the same:", len(audioFeatures) == len(links)))
-        print("{}{}".format("Amount of Audio Features:", len(audioFeatures)))
+        print("{}{}".format("Amount of links and audio features are the same:", len(audio_features) == len(links)))
+        print("{}{}".format("Amount of Audio Features:", len(audio_features)))
         print("{}{}".format("Amount of links:", len(links)))
-        print("{}{}".format("Start Index:", startIndex))
-        print("{}{}".format("End Index:", endIndex))
+        print("{}{}".format("Start Index:", start_index))
+        print("{}{}".format("End Index:", end_index))
         print "\n"
-    return audioFeatures
+    return audio_features
 
 
-def displayPlaylist(playlistID):
-    tracks = getTracksFromPlaylist(playlistID)
-    playlist = getPlaylistForID(playlistID)
+def display_playlist(playlist_id):
+    tracks = get_tracks_from_playlist(playlist_id)
+    playlist = get_playlist_for_id(playlist_id)
 
     # Print out playlist name
     print playlist['name']
@@ -131,89 +131,88 @@ def displayPlaylist(playlistID):
         print "\n"
 
 
-def writeAudioFeaturesToCSVFile(positiveAudioFeatures,negativeAudioFeatures):
+def write_audio_features_to_csv_file(positive_audio_features, negative_audio_features):
     # Create a new csv file
     with open('data.csv', 'wb') as csvfile:
         # Create a writer for the csv file
-        dataWriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        data_writer = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
         # Write a header for the CSV
-        dataWriter.writerow(['Acoustic',
-                             'Dance',
-                             'Energy',
-                             'Instrumental',
-                             'Live',
-                             'Speech',
-                             'Valence',
-                             'Label'])
+        data_writer.writerow(['Acoustic',
+                              'Dance',
+                              'Energy',
+                              'Instrumental',
+                              'Live',
+                              'Speech',
+                              'Valence',
+                              'Label'])
 
         # Create variables for tracking indices
-        negExampleIndex = 0
-        postiveExampleIndex = 0
+        negative_example_index = 0
+        positive_example_index = 0
 
         # Get size of datasets
-        postiveExampleSize = len(positiveAudioFeatures)
-        negativeExamplesSize = len(negativeAudioFeatures)
-
+        postive_example_size = len(positive_audio_features)
+        negative_examples_size = len(negative_audio_features)
 
         # While there are more positive data examples
-        while postiveExampleIndex < postiveExampleSize:
+        while positive_example_index < postive_example_size:
 
-            dataWriter.writerow([positiveAudioFeatures[postiveExampleIndex]['acousticness'],
-                                 positiveAudioFeatures[postiveExampleIndex]['danceability'],
-                                 positiveAudioFeatures[postiveExampleIndex]['energy'],
-                                 positiveAudioFeatures[postiveExampleIndex]['instrumentalness'],
-                                 positiveAudioFeatures[postiveExampleIndex]['liveness'],
-                                 positiveAudioFeatures[postiveExampleIndex]['speechiness'],
-                                 positiveAudioFeatures[postiveExampleIndex]['valence'], "In"])
-            postiveExampleIndex += 1
+            data_writer.writerow([positive_audio_features[positive_example_index]['acousticness'],
+                                 positive_audio_features[positive_example_index]['danceability'],
+                                 positive_audio_features[positive_example_index]['energy'],
+                                 positive_audio_features[positive_example_index]['instrumentalness'],
+                                 positive_audio_features[positive_example_index]['liveness'],
+                                 positive_audio_features[positive_example_index]['speechiness'],
+                                 positive_audio_features[positive_example_index]['valence'], "In"])
+            positive_example_index += 1
 
         # While there are more negative data examples
-        while negExampleIndex < negativeExamplesSize:
+        while negative_example_index < negative_examples_size:
             # Write certain data to the csv file
-            dataWriter.writerow([negativeAudioFeatures[negExampleIndex]['acousticness'],
-                                 negativeAudioFeatures[negExampleIndex]['danceability'],
-                                 negativeAudioFeatures[negExampleIndex]['energy'],
-                                 negativeAudioFeatures[negExampleIndex]['instrumentalness'],
-                                 negativeAudioFeatures[negExampleIndex]['liveness'],
-                                 negativeAudioFeatures[negExampleIndex]['speechiness'],
-                                 negativeAudioFeatures[negExampleIndex]['valence'], "Out"])
+            data_writer.writerow([negative_audio_features[negative_example_index]['acousticness'],
+                                 negative_audio_features[negative_example_index]['danceability'],
+                                 negative_audio_features[negative_example_index]['energy'],
+                                 negative_audio_features[negative_example_index]['instrumentalness'],
+                                 negative_audio_features[negative_example_index]['liveness'],
+                                 negative_audio_features[negative_example_index]['speechiness'],
+                                 negative_audio_features[negative_example_index]['valence'], "Out"])
 
-            negExampleIndex += 1
+            negative_example_index += 1
 
         print("Data successfully written to csv file")
 
 
-def parsePlaylistLink(playlistLink):
-    isPlaylist = False
-    creatorUsername = ""
-    playlistID = ""
+def parse_playlist_link(playlist_link):
+    is_playlist = False
+    creator_username = ""
+    playlist_id = ""
 
-    linkInfo = playlistLink[25:]
+    link_info = playlist_link[25:]
 
-    print "Link info:", linkInfo
+    print "Link info:", link_info
 
-    if linkInfo[:4] == "user":
-        isPlaylist = True
+    if link_info[:4] == "user":
+        is_playlist = True
         print "Link belongs to a playlist"
-        linkInfo = linkInfo[5:]
+        link_info = link_info[5:]
     else:
         print "Link does not belong to a playlist"
 
-    if isPlaylist:
-            for character in linkInfo:
+    if is_playlist:
+            for character in link_info:
                 if character is not "/":
-                    creatorUsername += character
+                    creator_username += character
                 else:
                     break
-            print "Username:", creatorUsername
+            print "Username:", creator_username
 
-            linkInfo = linkInfo[len(creatorUsername)+10:]
+            link_info = link_info[len(creator_username)+10:]
 
-            print "Link info without username:", linkInfo
-            playlistID = linkInfo
+            print "Link info without username:", link_info
+            playlist_id = link_info
             print "\n"
-    return isPlaylist, creatorUsername, playlistID
+    return is_playlist, creator_username, playlist_id
 
 
 """
@@ -223,26 +222,26 @@ Main Code
 scope = 'user-read-private user-read-email playlist-modify-private playlist-modify-public'
 
 # Get the ID's for the playlists of your positive and negative examples
-positiveExamplesPlaylistLink = "https://open.spotify.com/user/1228575772/playlist/3yizPHVVdByKDDman4yw4N"
-negativeExamplesPlaylistLink = "https://open.spotify.com/user/1228575772/playlist/36ez66SGOm2QAjPilCcd5m"
+positive_examples_playlist_link = "https://open.spotify.com/user/1228575772/playlist/3yizPHVVdByKDDman4yw4N"
+negative_examples_playlist_link = "https://open.spotify.com/user/1228575772/playlist/36ez66SGOm2QAjPilCcd5m"
 
 
 # Create username and Token objects
-authorizationUsername = getUsername()
-token = createTokenForScope(scope=scope, username=authorizationUsername)
+authorization_username = get_username()
+token = create_token_for_scope(scope=scope, username=authorization_username)
 
 # Create a Spotipy object
 sp = spotipy.Spotify(auth=token)
 
-isPosPlaylistLinkValid, posPlaylistUsername, posPlaylistID = parsePlaylistLink(positiveExamplesPlaylistLink)
-isNegPlaylistLinkValid, negPlaylistUsername, negPlaylistID = parsePlaylistLink(negativeExamplesPlaylistLink)
+is_pos_playlist_link_valid, pos_playlist_username, pos_playlist_id = parse_playlist_link(positive_examples_playlist_link)
+is_neg_playlist_link_valid, neg_playlist_username, neg_playlist_id = parse_playlist_link(negative_examples_playlist_link)
 
-if isPosPlaylistLinkValid:
-    if isNegPlaylistLinkValid:
-        positiveExamplesData = getAudioFeaturesForPlaylistID(posPlaylistID)
-        negativeExamplesData = getAudioFeaturesForPlaylistID(negPlaylistID)
-        writeAudioFeaturesToCSVFile(positiveAudioFeatures=positiveExamplesData,
-                                    negativeAudioFeatures=negativeExamplesData)
+if is_pos_playlist_link_valid:
+    if is_neg_playlist_link_valid:
+        positive_examples_data = get_audio_features_for_playlist_id(pos_playlist_id)
+        negative_examples_data = get_audio_features_for_playlist_id(neg_playlist_id)
+        write_audio_features_to_csv_file(positive_audio_features=positive_examples_data,
+                                         negative_audio_features=negative_examples_data)
     else:
         print "Negative playlist link is not valid"
 else:
